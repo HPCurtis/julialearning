dd823b0c46f7a8f0682570d79910ee0de47bddd3
Harrison <harrisoncurtis50@gmail.com>
2024-10-17 21:57:07 +0100

    Update fucntioning julia.jl pset0 code

--- cs50problemsets/pset0/faces.jl
+++ cs50problemsets/pset0/faces.jl
@@ -1,5 +1,12 @@
 function main()
-    print("U+1F642")
+    input = strip(readline())
+    input = replace(input, ":)" =>"\U0001F60A")
+    input = replace(input, ":(" => "\U0001F641")
+    # Prin.t the update output
+    println(input)
 end 
 
-main()
+# Runs main function if script is run directly from cli.
+if abspath(PROGRAM_FILE) == abspath(@__FILE__)
+    main()
+end
