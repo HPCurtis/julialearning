458d712ee58f2a4ff183d2f614bd2fed60e2c58e
Harrison <harrisoncurtis50@gmail.com>
2024-10-17 21:35:24 +0100

    updates to bitcoin.l code

--- cs50problemsets/pset4/bitcoin.jl
+++ cs50problemsets/pset4/bitcoin.jl
@@ -1,6 +1,32 @@
 using HTTP
+using JSON
+using Printf
 
 function main()
+    
+    # initialsise n
+    n = ""
+
+    if length(ARGS) < 1
+        println("Missing command-line argument")
+        exit()
+    end
+
+    try
+        n = parse(Float64, ARGS[1])
+    catch
+        println("Command-line argument is not a number")
+        exit()
+    end    
+
+    bitcoin = HTTP.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    data = JSON.parse(String(bitcoin.body))
+    USD_rate = data["bpi"]["USD"]["rate"]
+
+    cleaned_USD = replace(USD_rate, "," => "")
+    amount = parse(Float64, cleaned_USD) * n
+    println(amount)
+    
 end
 
 # Runs main function if script is run directly from cli.
